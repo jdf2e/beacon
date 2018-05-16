048f2ddae7025f832b0232c16a00124596d34504
liaoyanli5 <liaoyanli@jd.com>
2018-05-16 21:08:12 +0800

    20180516头部链接修改

--- web/includes/header.jsx
+++ web/includes/header.jsx
@@ -10,7 +10,7 @@ export default class Header extends React.Component {
      		<div className="header">
 				 <div className="w">
 					 <div className="l">
-						<Link to={'/'} className="logo">
+						<a href="//beacon.jd.com/" className="logo">
 							{/*<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" height="30" alt=""/>BeaconUI*/}
 
 	<svg version="1.1" id="图层_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
@@ -4369,7 +4369,7 @@ export default class Header extends React.Component {
 	</svg>
 
 
-						</Link>
+						</a>
 					</div>
 					<div className="r">
 						<a href="demo.html" className="link" target="_blank">查看M端demo</a>
